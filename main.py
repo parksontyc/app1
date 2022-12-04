fbe2a51c69a781dfa13764cab7bd882359cdaa79
parksontyc <parkson.tyc.chen@gmail.com>
2022-12-04 12:41:12 +0800

    day2

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 user_prompt = 'Enter a todo:'
-todo1 = input(user_prompt)
-todo2 = input(user_prompt)
 
-tools = [todo1, todo2, 'Hello']
-print(tools)
+while True:
+    todo = input(user_prompt)
+    print(todo)
+    print("Nest...")
+
